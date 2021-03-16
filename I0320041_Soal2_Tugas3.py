1860d0df0321ca69fc12f5a533ad75bf0a156b99
Fariduddin Attar Rahman <attarrahman461@gmail.com>
2021-03-16 15:16:20 +0700

    go

--- I0320041_Soal2_Tugas3.py
+++ I0320041_Soal2_Tugas3.py
@@ -1,5 +1,6 @@
 # Membuat dictionary
 dict = {'Nama':'Attar','Hobi1':'Futsal','Hobi2':'Belajar','Hobi3':'Membaca buku','Sosmed1':'ig:@4trmnn','Sosmed2':'line:akuattar','Sosmed3':'twitter:@akuattar','Lagu1':'Euphoria','Lagu2':'Past Life','Lagu3':'Monolog','Makanan1':'Magelangan','Makanan2':'Mie goreng','Makanan3':'Coklat'}
+
 # Mengubah salah satu hobi dan sosmed, hapus 2 makanan, dan tambah 1 hobi
 dict['Hobi2'] = 'Mengaji'
 dict['Sosmed3'] = 'ig:@indenture20'
