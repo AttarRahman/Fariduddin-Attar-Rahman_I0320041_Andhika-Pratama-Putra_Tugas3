1e2e28dade24d6274c246dd75028f274fd7b6ffd
Fariduddin Attar Rahman <attarrahman461@gmail.com>
2021-03-18 17:38:26 +0700

    go

--- I0320041_Soal1_Tugas3.py
+++ I0320041_Soal1_Tugas3.py
@@ -1,20 +1,20 @@
 # List 10 teman
-list = ['Bagus','Rio','Gea','Ara','Sasa','Rizal','Iqbal','Reza','Alif','Dito']
+teman = ['Bagus','Rio','Gea','Ara','Sasa','Rizal','Iqbal','Reza','Alif','Dito']
 
 # Menampilkan list indeks nomor 4,6, dan 7
-print("List indeks nomor 4,6, dan 7 adalah",list[4],",",list[6],", dan",list[7])
+print("List indeks nomor 4,6, dan 7 adalah",teman[4],",",teman[6],", dan",teman[7])
 
 # Mengganti nama teman di list 3,5, dan 9
-list[3] = 'Cancan'
-list[5] = 'Arya'
-list[9] = 'Rizky'
+teman[3] = 'Cancan'
+teman[5] = 'Arya'
+teman[9] = 'Rizky'
 
 # Menambahkan 2 nama teman
-list.append('Ayu')
-list.append('Rara')
+teman.append('Ayu')
+teman.append('Rara')
 
 # Menampilkan semua teman dengan pengulangan
-print(list*2)
+print(teman*2)
 
 # Menampilkan panjang list
-print(len(list))
+print(len(teman))
